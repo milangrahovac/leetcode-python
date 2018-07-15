f27f3021658fa31b97839f204388326443a4429d
Milan Grahovac <grahovac85@gmail.com>
2018-07-15 09:50:19 +0200

    add 709

---
+++ 709-to_lower_case.py
@@ -0,0 +1,8 @@
+# 709. To Lower Case
+#
+# Implement function ToLowerCase() that has a string parameter str,
+# and returns the same string in lowercase.
+
+class Solution:
+    def toLowerCase(self, str):
+        return str.lower()
