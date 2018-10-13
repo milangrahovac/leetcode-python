73258abbfd255e9ca6fc402dc3c6cae22c459d4a
Milan Grahovac <grahovac85@gmail.com>
2018-10-14 00:46:55 +0200

    fix test_917-reverse-only_letters.py

--- test_917-reverse-only_letters.py
+++ test_917-reverse-only_letters.py
@@ -1,9 +1,9 @@
 import unittest
-from test6 import Solution
+from 917-reverse-only_letters import Solution
 
 
 class TestCircleArea(unittest.TestCase):
     def test_area(self):
-        self.assertEqual(Solution().reverseOnlyLetters("ab-cd"), "dc-ba")
+        self.assertEqual(Solution().reverseOnlyLetters("ab-cd"), "dc-ba"))
         self.assertEqual(Solution().reverseOnlyLetters("a-bC-dEf-ghIj"), "j-Ih-gfE-dCba")
         self.assertEqual(Solution().reverseOnlyLetters("Test1ng-Leet=code-Q!"), "Qedo1ct-eeLg=ntse-T!")
