17751b578224db335bfad618efcb79bdc4b944f6
Milan Grahovac <grahovac85@gmail.com>
2018-11-02 21:54:09 +0100

    add 905

---
+++ 905-sort_array_by_parity.py
@@ -0,0 +1,26 @@
+# 905-sort_array_by_parity
+#
+# Given an array A of non-negative integers,
+# return an array consisting of all the even elements of A,
+# followed by all the odd elements of A.
+# You may return any answer array that satisfies this condition.
+#
+# Example 1:
+# Input: [3,1,2,4]
+# Output: [2,4,3,1]
+# The outputs [4,2,3,1], [2,4,1,3], and [4,2,1,3] would also be accepted.
+#
+# Note:
+# 1 <= A.length <= 5000
+# 0 <= A[i] <= 5000
+
+
+class Solution:
+    def sortArrayByParity(self, A):
+        even, odd = [], []
+        for x in A:
+            if x % 2 == 0:
+                even.append(x)
+            else:
+                odd.append(x)
+        return even + odd
