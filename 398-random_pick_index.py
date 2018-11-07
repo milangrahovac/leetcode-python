12338c9e587e2693f1a3d040168cd1c0ca0db635
Milan Grahovac <grahovac85@gmail.com>
2018-11-07 18:56:53 +0100

    add 398

---
+++ 398-random_pick_index.py
@@ -0,0 +1,41 @@
+# 398-random_pick_index
+#
+# Given an array of integers with possible duplicates,
+# randomly output the index of a given target number.
+# You can assume that the given target number must exist in the array.
+#
+# Note:
+# The array size can be very large.
+# Solution that uses too much extra space will not pass the judge.
+#
+# Example:
+# int[] nums = new int[] {1,2,3,3,3};
+# Solution solution = new Solution(nums);
+#
+# pick(3) should return either index 2, 3, or 4 randomly.
+# Each index should have equal probability of returning.
+# solution.pick(3);
+#
+# pick(1) should return 0. Since in the array only nums[0] is equal to 1.
+# solution.pick(1);
+#
+# --------------------------------------------------------------
+# Your Solution object will be instantiated and called as such:
+# obj = Solution(nums)
+# param_1 = obj.pick(target)
+# ---------------------------------------------------------------
+
+import random
+
+
+class Solution(object):
+
+    def __init__(self, nums):
+        self.nums = nums
+
+    def pick(self, target):
+        result = []
+        for i in range(len(self.nums)):
+            if target == self.nums[i]:
+                result.append(i)
+        return random.choice(result)
