503f1c109c685d98eb56a314d09afb27d36ee000
Milan Grahovac <grahovac85@gmail.com>
2018-11-15 22:40:49 +0100

    add 190

--- 190-reverse_bits.py
+++ 190-reverse_bits.py
@@ -8,8 +8,6 @@
 # Output: 964176192
 # Explanation: 43261596 represented in binary as 00000010100101000001111010011100,
 #              return 964176192 represented in binary as 00111001011110000010100101000000.
-#
-#
 
 
 class Solution:
