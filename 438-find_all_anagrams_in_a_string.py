e88172763c0d4a41b6f373e25b073fd2151c463b
Milan Grahovac <grahovac85@gmail.com>
2018-11-20 22:33:42 +0100

    update 438

--- 438-find_all_anagrams_in_a_string.py
+++ 438-find_all_anagrams_in_a_string.py
@@ -5,22 +5,16 @@
 # The order of output does not matter.
 #
 # Example 1:
-#
-# Input:
-# s: "cbaebabacd" p: "abc"
-# Output:
-# [0, 6]
+# Inputs: s: "cbaebabacd" p: "abc"
+# Output: [0, 6]
 #
 # Explanation:
 # The substring with start index = 0 is "cba", which is an anagram of "abc".
 # The substring with start index = 6 is "bac", which is an anagram of "abc".
 #
 # Example 2:
-#
-# Input:
-# s: "abab" p: "ab"
-# Output:
-# [0, 1, 2]
+# Inputs: s: "abab" p: "ab"
+# Output: [0, 1, 2]
 #
 # Explanation:
 # The substring with start index = 0 is "ab", which is an anagram of "ab".
