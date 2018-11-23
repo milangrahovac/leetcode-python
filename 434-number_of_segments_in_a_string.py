f15dd9be44cef0e4f98245236a665b22eb04b9c1
Milan Grahovac <grahovac85@gmail.com>
2018-11-23 22:50:55 +0100

    add 434

--- 434-number_of_segments_in_a_string.py
+++ 434-number_of_segments_in_a_string.py
@@ -1,11 +1,11 @@
 # 434. Number of Segments in a String
 #
-# Count the number of segments in a string, where a segment is defined to be a contiguous sequence of non-space characters.
-#
+# Count the number of segments in a string, 
+# where a segment is defined to be a contiguous sequence of non-space characters.
+# 
 # Please note that the string does not contain any non-printable characters.
 #
 # Example:
-#
 # Input: "Hello, my name is John"
 # Output: 5
 
@@ -13,4 +13,3 @@
 class Solution(object):
     def countSegments(self, s):
         return len(s.split())
-
