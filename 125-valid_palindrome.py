68b29c84bbc43cfccdc9da34bf7dde7727e44cb7
Milan Grahovac <grahovac85@gmail.com>
2018-12-09 08:51:47 +0100

    update 125

--- 125-valid_palindrome.py
+++ 125-valid_palindrome.py
@@ -1,2 +1,16 @@
 125. Valid Palindrome
 
+class Solution(object):
+    def isPalindrome(self, s):
+
+        new_string = ""
+        alpha = string.ascii_letters
+        nums = string.digits
+
+        for x in s:
+            if x in alpha:
+                new_string += x.lower()
+            elif x in nums:
+                new_string += x
+
+        return True if new_string == new_string[::-1] else False
