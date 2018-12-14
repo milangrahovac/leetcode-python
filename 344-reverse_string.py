a229bd24a12fe0e01b618f12c87b1ec2126b1c9d
Milan Grahovac <grahovac85@gmail.com>
2018-12-14 23:08:16 +0100

    add 344

--- 344-reverse_string.py
+++ 344-reverse_string.py
@@ -3,11 +3,10 @@
 # Write a function that takes a string as input and returns the string reversed.
 
 # Example 1:
-
 # Input: "hello"
 # Output: "olleh"
-# Example 2:
 
+# Example 2:
 # Input: "A man, a plan, a canal: Panama"
 # Output: "amanaP :lanac a ,nalp a ,nam A"
 
@@ -15,4 +14,3 @@
 class Solution(object):
     def reverseString(self, s):
         return s[::-1]
-        
