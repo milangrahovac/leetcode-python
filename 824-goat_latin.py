628a9f895cd817b402ba6e785b17dae44b89c38a
Milan Grahovac <grahovac85@gmail.com>
2018-12-16 10:10:46 +0100

    fix description 824

--- 824-goat_latin.py
+++ 824-goat_latin.py
@@ -8,11 +8,13 @@
 # If a word begins with a vowel (a, e, i, o, or u), append "ma" to the end of the word.
 # For example, the word 'apple' becomes 'applema'.
 #
-# If a word begins with a consonant (i.e. not a vowel), remove the first letter and append it to the end, then add "ma".
+# If a word begins with a consonant (i.e. not a vowel), 
+# remove the first letter and append it to the end, then add "ma".
 # For example, the word "goat" becomes "oatgma".
 #
 # Add one letter 'a' to the end of each word per its word index in the sentence, starting with 1.
-# For example, the first word gets "a" added to the end, the second word gets "aa" added to the end and so on.
+# For example, the first word gets "a" added to the end, 
+# the second word gets "aa" added to the end and so on.
 # Return the final sentence representing the conversion from S to Goat Latin.
 #
 #
