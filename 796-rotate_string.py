485a7caeb3b4c8d8e7f151d6d81445b6018135f8
Milan Grahovac <grahovac85@gmail.com>
2018-12-18 20:26:59 +0100

    add 796

--- 796-rotate_string.py
+++ 796-rotate_string.py
@@ -1,16 +1,18 @@
-796. Rotate String
+# 796. Rotate String
 
-We are given two strings, A and B.
+# We are given two strings, A and B.
 
-A shift on A consists of taking string A and moving the leftmost character to the rightmost position. For example, if A = 'abcde', then it will be 'bcdea' after one shift on A. Return True if and only if A can become B after some number of shifts on A.
+# A shift on A consists of taking string A and moving the leftmost character to the rightmost position. 
+# For example, if A = 'abcde', then it will be 'bcdea' after one shift on A. 
+# Return True if and only if A can become B after some number of shifts on A.
 
-Example 1:
-Input: A = 'abcde', B = 'cdeab'
-Output: true
+# Example 1:
+# Input: A = 'abcde', B = 'cdeab'
+# Output: true
 
-Example 2:
-Input: A = 'abcde', B = 'abced'
-Output: false
-Note:
+# Example 2:
+# Input: A = 'abcde', B = 'abced'
+# Output: false
 
-A and B will have length at most 100.
+# Note:
+# A and B will have length at most 100.
