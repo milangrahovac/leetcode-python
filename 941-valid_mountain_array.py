6b617580f1d336af49db7d436e0fac20d86ce14c
Milan Grahovac <grahovac85@gmail.com>
2019-01-10 22:15:36 +0100

    add 941

--- 941-valid_mountain_array.py
+++ 941-valid_mountain_array.py
@@ -1,7 +1,6 @@
 # 941. Valid Mountain Array
 
 # Given an array A of integers, return true if and only if it is a valid mountain array.
-
 # Recall that A is a mountain array if and only if:
 
 # A.length >= 3
