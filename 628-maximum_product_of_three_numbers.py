347c713303ede06254e7d42db1fe0e415252859e
Milan Grahovac <grahovac85@gmail.com>
2019-01-20 15:55:44 +0100

    update 628 description

--- 628-maximum_product_of_three_numbers.py
+++ 628-maximum_product_of_three_numbers.py
@@ -12,7 +12,7 @@
 #
 # Note:
 # The length of the given array will be in range [3,104] and all elements are in the range [-1000, 1000].
-#     Multiplication of any three numbers in the input won't exceed the range of 32-bit signed integer.
+# Multiplication of any three numbers in the input won't exceed the range of 32-bit signed integer.
 
 
 class Solution(object):
