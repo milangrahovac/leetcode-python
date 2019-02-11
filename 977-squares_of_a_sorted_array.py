351a6f9b02bc4f804a2bac0d51e7936662e4d47d
Milan Grahovac <grahovac85@gmail.com>
2019-02-11 15:42:19 +0100

    add 977

--- 977-squares_of_a_sorted_array.py
+++ 977-squares_of_a_sorted_array.py
@@ -2,7 +2,6 @@
 
 # Given an array of integers A sorted in non-decreasing order, 
 # return an array of the squares of each number, also in sorted non-decreasing order.
-
  
 # Example 1:
 # Input: [-4,-1,0,3,10]
@@ -12,7 +11,6 @@
 # Input: [-7,-3,2,3,11]
 # Output: [4,9,9,49,121]
  
-
 # Note:
 # 1 <= A.length <= 10000
 # -10000 <= A[i] <= 10000
