253e4a19a0250b6dc03bca88eee9dc2668a3c024
Milan Grahovac <grahovac85@gmail.com>
2019-03-16 17:40:51 +0100

    add description 215

--- 215-kth_largest_element_in_an_array.py
+++ 215-kth_largest_element_in_an_array.py
@@ -1,6 +1,7 @@
 # 215. Kth Largest Element in an Array
 
-# Find the kth largest element in an unsorted array. Note that it is the kth largest element in the sorted order, not the kth distinct element.
+# Find the kth largest element in an unsorted array. 
+# Note that it is the kth largest element in the sorted order, not the kth distinct element.
 
 # Example 1:
 # Input: [3,2,1,5,6,4] and k = 2
