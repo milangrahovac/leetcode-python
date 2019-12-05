d1beccca1020d74832e9e116967a6d1c73cbd00f
Milan Grahovac <milangrahovac@Milans-MBP.fritz.box>
2019-12-05 22:25:03 +0100

    add 1207

--- 1207-unique_number_of_occurrences.py
+++ 1207-unique_number_of_occurrences.py
@@ -1,1 +1,12 @@
-1207. Unique Number of Occurrences
+# 1207. Unique Number of Occurrences
+
+class Solution(object):
+    def uniqueOccurrences(self, arr):
+        c, l = None, []
+        for x in set(arr):
+            c = arr.count(x)
+            if c not in l:
+                l.append(c)
+            else:
+                return False
+        return True
